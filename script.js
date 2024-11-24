fb6ee87fd89a385939ce154d4b571b288ef120e1
MartialFlamme <ketchadjiguymartial@gmail.com>
2024-11-24 14:18:34 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+"use strict";
 function appendToDisplay(value) {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value += value;
 }
 function clearDisplay() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = '';
 }
 function calculateResult() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     try {
         display.value = eval(display.value).toString();
     }
